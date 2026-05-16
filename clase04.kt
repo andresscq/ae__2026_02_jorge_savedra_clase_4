435278aea46a2c151d1018580ee0c3a6a7dea703
andresscq <ivancriollo3@gmail.com>
2026-05-16 09:05:48 -0500

    Tercer commit clase 04

--- clase04.kt
+++ clase04.kt
@@ -5,15 +5,33 @@
 
 
 fun main() {
+
     val name: String = "Andres"
     val lastName: String = "Criollo"
 
     println("Hello $name $lastName!!!")
+
+    val student = Student(
+        id = 1,
+        name = "Ana",
+        email = "Ana@gmail.com",
+        grade = 18,
+        isActive = false
+    )
+
+    println(student)
+
+    if (student.isActive) {
+        println("El estudiante ${student.name} esta activo")
+    } else {
+        println("El estudiante ${student.name} NO esta activo")
+    }
 }
 
-data class Student (
+data class Student(
     val id: Long,
-    val name: String
-    val email: String
-
+    val name: String,
+    val email: String,
+    val grade: Int,
+    val isActive: Boolean
 )
