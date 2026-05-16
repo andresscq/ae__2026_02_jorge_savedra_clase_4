f131f20bed6e10f8843cca62dbb6e899ad1bc821
andresscq <ivancriollo3@gmail.com>
2026-05-16 08:49:52 -0500

    Primer commit clase 04

---
+++ clase04.kt
@@ -0,0 +1,12 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+
+fun main() {
+    val name: String = "Andres"
+    val lastName: String = "Criollo"
+
+    println("Hello $name $lastName!!!")
+}
